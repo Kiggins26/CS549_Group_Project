b0c70f7300fa060e7d82f34e097962b0682a9d1e
Andrew Kiggins <30563055+Kiggins26@users.noreply.github.com>
2024-02-27 14:35:48 +0000

    Update sql_query.sql

--- sql_query.sql
+++ sql_query.sql
@@ -1,7 +1,7 @@
 --1. List the ID and name of all employees.
 SELECT id, name FROM Employees;
 --2. List the name of all projects together with their type
-SELECT name, type FROM Projects GROUP BY type; 
+SELECT name, type FROM Projects; 
 --3. List the ID and name of all employees together with the project name and project category that they work on.
 SELECT Employees.id, Employees.name, Projects.name, Projects.category FROM Employees INNER JOIN Employees.team_id = Projects.team_id GROUP BY Projects.category, Projects.name;
 --4. List the ID and name of all tasks together with the name and ID of the employee who works on this task
