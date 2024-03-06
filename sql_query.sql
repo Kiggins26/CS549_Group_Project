ad6d8b9fe726637c321694e08a9e7621e1b9a0c0
Vishesh Kishore <visheshkishore@Visheshs-MacBook-Air.local>
2024-03-06 11:32:22 +0000

    queries updated

--- sql_query.sql
+++ sql_query.sql
@@ -7,10 +7,10 @@ select id, name from Employees;
 select distinct(Name), type from Projects;
 
 -- 3. List the ID and name of all employees together with the project name and project category that they work on.
-select Employees.id, Employees.name, Projects.Name, Projects.Category from Employees, Projects where Employees.team_id = Projects.team_id;
+select Employees.emp_id, Employees.name, Projects.Name, Projects.Category from Employees, Projects where Employees.team_id = Projects.team_id;
 
 -- 4. List the ID and name of all tasks together with the name and ID of the employee who works on this task.
-select Tasks.Code, Tasks.Name, Employees.id, Employees.Name from Tasks, Employees where Tasks.emp_id = Employees.id;
+select Tasks.Code, Tasks.Name, Employees.emp_id, Employees.Name from Tasks, Employees where Tasks.emp_id = Employees.emp_id;
 
 -- 5. List the ID and name of all employees together with the office number that they are based in.
 select id, name, office_location from Employees;
